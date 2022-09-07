1d101549666bf2eab65b47bc80058d9e98909b29
unknown <lukacc008@gmail.com>
2022-09-08 00:54:34 +0200

    Setup for sending POST request to firebase DB

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-6e5fd-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
